1f831dbb2f4d922b7defc93fdfec5f7e9d8c5d91
Rickard Armiento <rickard-gpg@armiento.net>
2026-03-27 00:59:35 +0000

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from datetime import date
+
+sys.path.insert(0, os.path.abspath("../src"))
+
+project = "httk-web"
+author = "The httk-web AUTHORS"
+copyright = f"{date.today().year}, {author}"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
+
+autosummary_generate = True
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "undoc-members": False,
+    "show-inheritance": True,
+}
+
+html_theme = "furo"
+html_theme_options = {
+    "sidebar_hide_name": False,
+    "navigation_with_keys": True,
+}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+httk-web
+========
+
+``httk-web`` provides web serving and static publishing functionality for ``httk`` v2.
+
+Quick links
+-----------
+
+- :doc:`how_it_works`
+- :doc:`reference`
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Documentation
+
+   how_it_works
+   reference
